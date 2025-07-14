c621c9372766b877e6e78127f4b1d7d93c4f5558
Shashank Manohar <shashankmanohar1734@gmail.com>
2025-07-14 18:57:00 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:4002/api',
+  baseURL: 'https://app-backend-phi.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
